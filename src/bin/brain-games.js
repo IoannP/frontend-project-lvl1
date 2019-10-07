fc0158765d15299c5a3d2c67ae60faa60f807acd
giohann <giohann@yahoo.com>
2019-10-08 01:49:54 +0300

    adding readline-sync

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
+import index from '..'
+
 console.log('Welcome to the Brain Games!');
+
+var readlineSync = require('readline-sync');
+
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hello ' + userName + '!');
+
