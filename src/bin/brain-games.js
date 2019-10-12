5b16189d8e375fd4f9cf7e22ed269aa31330dbb0
giohann <giohann@yahoo.com>
2019-10-12 14:27:39 +0300

    correcting of .js files

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
 import index from '..';
+
+console.log(`Hi ${index}!`);
--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ import readlineSync from 'readline-sync';
 console.log('Welcom to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
-console.log(`Hi ${userName}!`);
+
+export default userName;
